9488a47602d787c1054300b051e47fad5d4059b1
Alelgn-Masresha <alxmax071@gmail.com>
2025-09-23 16:08:08 +0300

    add cors parameters

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:5173", "https://packaging-system.vercel.app"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
