2670e50d69cb24178b3d19c550cc405ff65d070b
Alelgn-Masresha <alxmax071@gmail.com>
2025-09-23 17:01:53 +0300

    api.ts change

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,7 @@
-const API_BASE_URL: string = 'http://localhost:5000/api';
+const API_BASE_URL: string =
+  import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api';
+
+
 
 class ApiService {
   baseURL: string;
